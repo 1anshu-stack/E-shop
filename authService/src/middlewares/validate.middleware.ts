f066869e37d7c6137813187f9658cf8686e6a7d3
1anshu-stack <anshuu424@gmail.com>
2026-02-11 12:09:04 +0530

    change prisma schema and generate migration folder for refresh token

--- authService/src/middlewares/validate.middleware.ts
+++ authService/src/middlewares/validate.middleware.ts
@@ -1,7 +1,5 @@
 import { ZodObject } from 'zod';
 import { Request, Response, NextFunction } from 'express';
-import { ParamsDictionary } from 'express-serve-static-core';
-import { ParsedQs } from 'qs';
 import { ValidationError } from '../utils/validationError';
 
 
