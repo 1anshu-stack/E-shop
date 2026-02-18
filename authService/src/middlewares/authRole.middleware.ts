9ed1855d92f998c2c7ea015034acfc1c6fc849e5
1anshu-stack <anshuu424@gmail.com>
2026-02-18 14:48:36 +0530

    solve the issue with a request object

--- authService/src/middlewares/authRole.middleware.ts
+++ authService/src/middlewares/authRole.middleware.ts
@@ -2,11 +2,18 @@ import { Request, Response, NextFunction } from "express";
 import { Forbidden } from "../utils/httpErrors";
 
 
+interface AuthenticatedRequest extends Request {
+  user: {
+    id: string;
+    role: string;
+  };
+}
+
 export const roleBasedAuth = (
   ...roles: string[]
 ) => {
-  return(req: Request, res: Response, next: NextFunction) => {
-    if(!roles.includes(req?.user.role)){
+  return(req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    if(!roles.includes(req.user.role)){
       throw Forbidden("Access denied")
     }
 
