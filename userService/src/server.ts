1899347902858635c6a0a0da8278eca89f737309
1anshu-stack <anshuu424@gmail.com>
2026-02-19 13:39:25 +0530

    add dotenv in server.ts file and add zod validator for user

--- userService/src/server.ts
+++ userService/src/server.ts
@@ -1,5 +1,8 @@
+import { dot } from "node:test/reporters";
 import app from "./app";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 app.listen(process.env.PORT, () => {
   console.log(`user server start on port ${process.env.PORT}`)
