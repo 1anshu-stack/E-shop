9d9fdeb4cea1a11382dcee6942af3ea5ec222452
1anshu-stack <anshuu424@gmail.com>
2026-02-19 12:27:33 +0530

    add basic files create prisma schema and migrate it

---
+++ userService/prisma/migrations/20260219064551_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "UserProfile" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "phone" TEXT,
+    "avatar" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserProfile_userId_key" ON "UserProfile"("userId");
+
+-- CreateIndex
+CREATE INDEX "UserProfile_userId_idx" ON "UserProfile"("userId");
