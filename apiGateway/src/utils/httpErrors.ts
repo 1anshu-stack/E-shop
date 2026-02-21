a8fc5d49ae76efd920cef2be0c7ef14fb21bcded
1anshu-stack <anshuu424@gmail.com>
2026-02-21 13:59:56 +0530

    create an rate limiter

--- apiGateway/src/utils/httpErrors.ts
+++ apiGateway/src/utils/httpErrors.ts
@@ -6,3 +6,4 @@ export const Unauthorized = (msg: string) => new AppError(msg, 401);
 export const Forbidden = (msg: string) => new AppError(msg, 403);
 export const NotFound = (msg: string) => new AppError(msg, 404);
 export const validationFailed = (msg: string) => new AppError(msg, 422)
+export const TooManyRequest = (msg: string) => new AppError(msg, 429)
