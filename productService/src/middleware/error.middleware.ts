4e9c2eac2750a323348e4bebac42ba01502a2fba
1anshu-stack <anshuu424@gmail.com>
2026-02-22 16:33:09 +0530

    create a global error middleware, centralized appError and httpError

---
+++ productService/src/middleware/error.middleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/appError";
+
+
+
+export const globalError = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if(err instanceof AppError){
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message
+    })
+  }
+
+  console.error("UNEXPECTED ERROR:", err);
+
+  
+  return res.status(500).json({
+    success: false,
+    message: "Something went wrong. Please try again later"
+  })
+}
