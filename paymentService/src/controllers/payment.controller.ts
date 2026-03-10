5276339c431233c773e8a3a40388971595062412
1anshu-stack <anshuu424@gmail.com>
2026-03-10 12:37:53 +0530

    payment controller

--- paymentService/src/controllers/payment.controller.ts
+++ paymentService/src/controllers/payment.controller.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from "express";
+import { asyncHandler } from "../utils/asyncHandler";
 
-export const checkout = () => {
-  
-}
+import * as paymentService from "../services/payment.service";
+
+
+export const createPaymentController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const {orderId, userId, amount} = req.body;
+    const result = await paymentService.createPayment(orderId, userId, amount);
+
+    res.status(200).json({
+      status: true,
+      message: "Payment create but in pending state",
+      data: result
+    })
+  }
+)
